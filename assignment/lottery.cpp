51ec70983bf6c10bfa43f26426c49fdb34da533f
Siamashka Vlad <wladsemashko@gmail.com>
2019-04-11 11:36:16 +0200

    :)

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -19,21 +19,21 @@
 
 
 bool init_lottery(const char *csv_file, char csv_separator){
-
+  return fopen(csv_file, "r")!=0;
 }
 
 bool get_tip(int tip_number, int tip[TIP_SIZE]){
-
+ return false;
 }
 
 bool set_drawing(int drawing_numbers[TIP_SIZE]){
-
+  return false;
 }
 
 int get_tip_result(int tip_number){
-
+  return 0;
 }
 
 int get_right_tips_count(int right_digits_count){
-
+  return 0;
 }
